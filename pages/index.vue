9be79d3474a1616885e0a99b4e968b927aca7617
yushimasu <yellow.box0803@gmail.com>
2021-04-26 22:30:52 +0900

    インスタ画像幅修正

--- pages/index.vue
+++ pages/index.vue
@@ -24,14 +24,9 @@
       <p>{{ name }}</p>
       <ul class="resultItems">
         <li class="resultItem" v-for="post in results" :key="post.id">
-          <img :src="post.media_url" class="imgStyle" />
-          <dl class="dataStyle">
-            <dt>
-              <a :href="post.permalink">
-                {{ post.caption }}
-              </a>
-            </dt>
-          </dl>
+          <a :href="post.permalink"  target="_blank" rel="noopener noreferrer">
+            <img :src="post.media_url" class="imgStyle" />
+          </a>
         </li>
       </ul>
     </div>
@@ -43,11 +38,11 @@ export default {
   data() {
     return {
       results: [],
-      name: 'masu',
+      name: "masu",
     };
   },
   async asyncData({ app }) {
-    const baseUrl = process.env. BASE_URL;
+    const baseUrl = process.env.BASE_URL;
     const response = await app.$axios.$get(baseUrl);
     return {
       results: response.media.data,
@@ -65,7 +60,6 @@ export default {
   align-items: center;
   text-align: center;
 }
-
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -75,7 +69,6 @@ export default {
   color: #35495e;
   letter-spacing: 1px;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -83,8 +76,26 @@ export default {
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
 .links {
   padding-top: 15px;
+}
+.imgStyle {
+  width: 100%;
+  display: block;
+}
+.resultItems {
+  display: flex;
+  padding: 0;
+  flex-direction: row;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  width: 50%;
+  margin: 0 auto;
+}
+.resultItem {
+  width: 33.3%;
+}
+ul {
+  list-style: none;
 }
 </style>
