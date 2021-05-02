ec01565913f70280fe96c2fd9b7e228077913741
yushimasu <yellow.box0803@gmail.com>
2021-05-02 14:36:43 +0900

    gsap install

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,7 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['gsap']
   },
   env: {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000'
