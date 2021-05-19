a5d12610f3323d59eeab59bd693821674af2969a
yushimasu <yellow.box0803@gmail.com>
2021-05-19 22:08:04 +0900

    microCMSの導入

---
+++ components/Works/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul class="works_list">
+      <li v-for="content in contents" :key="content.id">
+        <h3>{{ content.title }}</h3>
+        <img :src="content.image.url" />
+        <div class="post" v-html="content.body"></div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default ({
+  props: {
+    contents: {
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped>
+.works_list li{
+  width: 30%;
+}
+.works_list li img{
+  max-width: 100%;
+  display: block;
+}
+</style>
---
+++ pages/works/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+
+export default {
+  async asyncData({ app,params }) {
+    const { data } = await app.$axios.get(
+      `https://yuppies.microcms.io/api/v1/works/${params.slug}`,
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { "X-API-KEY": "ff3b2067-ed84-46ac-88ec-39d4896271a0" },
+      }
+    )
+    return data
+  }
+}
+</script>
