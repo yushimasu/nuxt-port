98b430d26026f551d1b24a95177297a4a3bb7365
yushimasu <yellow.box0803@gmail.com>
2021-05-22 16:38:22 +0900

    実績紹介ページ作成

---
+++ pages/works/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $config,app,params }) {
+    const { data } = await app.$axios.get(
+      `https://yuppies.microcms.io/api/v1/works/${params.slug}`,
+      {
+        headers: { "X-API-KEY": $config.microcmsAPI },
+      }
+    )
+    console.log(data)
+    return data
+  }
+}
+</script>
